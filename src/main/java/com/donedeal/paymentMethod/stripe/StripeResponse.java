38ad628290b9f5302c52bac14e9476788d0535c6
ycrangelo <yocorangelo18@gmail.com>
2025-07-26 23:02:40 +0800

    added stripe payment method,returns success and failed

---
+++ src/main/java/com/donedeal/paymentMethod/stripe/StripeResponse.java
@@ -0,0 +1,19 @@
+package com.donedeal.schema.stripe;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StripeResponse {
+
+    private String status;
+    private String message;
+    private String sessionId;
+    private String sessionUrl;
+}
